2b1fd13c240d83ee881af393ad0c841857b78850
msuzdaleu <Svetlana1972>
2017-11-09 11:27:10 +0300

    Стили для карточек

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule} from '@angular/core';
-import { MatCardModule } from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {MatCardModule} from '@angular/material';
 import {MatButtonModule} from '@angular/material';
 
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { VideoService } from './services/video.service';
-import { SearchComponent } from './components/search/search.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ItemComponent } from './components/item/item.component';
-import { MainComponent} from './components/main.component';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {VideoService} from './services/video.service';
+import {SearchComponent} from './components/search/search.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {ItemComponent} from './components/item/item.component';
+import {MainComponent} from './components/main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchComponent,
-    DashboardComponent,
-    ItemComponent,
-    MainComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpModule,
-    MatCardModule,
-    MatButtonModule
-  ],
-  providers: [VideoService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SearchComponent,
+        DashboardComponent,
+        ItemComponent,
+        MainComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpModule,
+        MatCardModule,
+        MatButtonModule
+    ],
+    providers: [VideoService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,24 +1,25 @@
-import {Component,  OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {VideoService} from '../../services/video.service';
 import {MainComponent} from '../main.component';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-item',
-  templateUrl: 'item.component.html',
-  styleUrls: ['item.component.css']
+    moduleId: module.id,
+    selector: 'app-item',
+    templateUrl: 'item.component.html',
+    styleUrls: ['item.component.css']
 })
 
 export class ItemComponent implements OnInit {
 
-  videos: any[];
-  constructor(private services: VideoService) {
-  }
+    videos: any[];
 
-  ngOnInit() {
-    MainComponent.onButtonClick.subscribe(() => {
-      this.videos = null;
-      this.videos =  this.services.mas;
-    });
-  }
+    constructor(private services: VideoService) {
+    }
+
+    ngOnInit() {
+        MainComponent.onButtonClick.subscribe(() => {
+            this.videos = null;
+            this.videos = this.services.mas;
+        });
+    }
 }
