de1b1fd638aaee24e8cf448110e946771f89c5ec
msuzdaleu <Svetlana1972>
2017-11-09 10:13:54 +0300

    Хранение запроса

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,29 +1,30 @@
-import {Component,  OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {VideoService} from '../../services/video.service';
 import {MainComponent} from '../main.component'
 
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-item',
-  templateUrl: 'item.component.html',
-  styleUrls: ['item.component.css']
+    moduleId: module.id,
+    selector: 'app-item',
+    templateUrl: 'item.component.html',
+    styleUrls: ['item.component.css']
 })
 export class ItemComponent implements OnInit {
 
-  video: Observable<any> ;
-  title: string = '';
-  videos: any[];
-  constructor(private services: VideoService) {
-  }
+    video: Observable<any>;
+    title: string = '';
+    videos: any[];
 
-  ngOnInit() {
-    MainComponent.onButtonClick.subscribe(() => {
-      this.videos = null;
-      this.videos =  this.services.mas;
-      console.log("!!!", this.videos);
-    });
-  };
+    constructor(private services: VideoService) {
+    }
+
+    ngOnInit() {
+        MainComponent.onButtonClick.subscribe(() => {
+            this.videos = null;
+            this.videos = this.services.mas;
+            console.log("!!!", this.videos);
+        });
+    };
 
 }
--- src/app/components/main.component.ts
+++ src/app/components/main.component.ts
@@ -1,27 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {VideoService} from '../services/video.service';
 import {Observable} from 'rxjs/Observable';
 import {Subject} from "rxjs/Subject";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.css']
 })
 
 export class MainComponent {
-  title = 'Поиск видео на ютуб';
-  videos: Observable<any>;
-  static onButtonClick: any = new Subject();
+    title = 'Поиск видео на ютуб';
+    videos: Observable<any>;
+    static onButtonClick: any = new Subject();
 
-  constructor(private service: VideoService) { }
+    constructor(private service: VideoService) {
+    }
 
-  pass(event) {
-    this.service.getVideo(event)
-      .subscribe(video => {
-        this.videos = video;
-        MainComponent.onButtonClick.next();
-        console.log('main', this.videos);
-      });
-  }
+    pass(event) {
+        this.service.getVideo(event)
+            .subscribe(video => {
+                this.videos = video;
+                MainComponent.onButtonClick.next();
+                console.log('main', this.videos);
+            });
+    }
 }
